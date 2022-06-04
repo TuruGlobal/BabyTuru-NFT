831ef4d8720be2ef7845dab5598ab0bd4876aee5
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:05:30 +0200

    Create 23.js

---
+++ api/BabyTuru/23.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT23 Capt J Calugas",
+    "image" : "https://gateway.btfs.io/btfs/QmS43D6KXkBwWiqKFwzFXJcVXt6iUgY4rqctfJ4HBe41Ab"
+  })
+}
