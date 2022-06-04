abf9b9c51d5960a8e55bbd3db282e1eaa5d3e9f3
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:55:51 +0200

    Create 33.js

---
+++ api/BabyTuru/33.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT33 Johan Cruyff",
+    "image" : "https://gateway.btfs.io/btfs/QmTf7KMvBvBLPoBYfaEpP2Nv7vkrCF9BobJCkSjfmbZpEQ"
+  })
+}
