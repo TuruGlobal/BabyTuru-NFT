01d02fb6e530a175102b714802f5a52d9272a833
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:29:49 +0200

    Create 3.js

---
+++ api/BabyTuru/3.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT3 Georgie",
+    "image" : "https://gateway.btfs.io/btfs/QmeKna7cbkC9NQQA8vWwSCDrzCr3Uqpj6X76YzEbuAeyMM"
+  })
+}
