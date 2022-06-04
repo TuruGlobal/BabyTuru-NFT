0aa0576cf1c3ad391603f57e0153db86d168ade5
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:39:52 +0200

    Create 10.js

---
+++ api/BabyTuru/10.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT10 Zen Gnarly",
+    "image" : "https://gateway.btfs.io/btfs/QmVSjmURhrnzMJEy2awpF3D7TVPotJh4VHyzwNCL4izApf"
+  })
+}
