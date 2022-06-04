23f94280b974adf6482a6c74bb3280f81de8012b
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:51:53 +0200

    Create 15.js

---
+++ api/BabyTuru/15.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT15 Antski",
+    "image" : "https://gateway.btfs.io/btfs/QmTAEcySD8hhwq5i3ywu6kFfUUbGm2kDCJWMpYXTtJVRC1"
+  })
+}
