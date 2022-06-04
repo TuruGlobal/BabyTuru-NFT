04ec7aad5c5080e25cc89f81dbb3e91b7794c735
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:34:01 +0200

    Create 28.js

---
+++ api/BabyTuru/28.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT28 Dawnie",
+    "image" : "https://gateway.btfs.io/btfs/QmdmoBu3P4VH15Evg9MckmPjZLSoLDMeWFt3M7CRHxuKHt"
+  })
+}
