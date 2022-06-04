01461095843ddb29d3a186e2868a79f01574b9cf
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:00:43 +0200

    Create 21.js

---
+++ api/BabyTuru/21.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT21 MoneyVersac",
+    "image" : "https://gateway.btfs.io/btfs/QmP9rkAXthG3fXPWEBWTfFmHQddgx7FJvAobPZ42G1z3b7"
+  })
+}
