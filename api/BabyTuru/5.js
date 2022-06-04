fed51c0e21dbd3b985eeb075f1f5e516f189a95e
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:32:21 +0200

    Create 5.js

---
+++ api/BabyTuru/5.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT5 Dandie",
+    "image" : "https://gateway.btfs.io/btfs/QmfFj5qRy5xRd5spDknEEoD66ViZ3XhUWSQL9EcvLnbP4V"
+  })
+}
