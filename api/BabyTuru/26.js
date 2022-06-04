4b2c9e70f93a695e8153124c5ca2263c73fc1d6d
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:31:36 +0200

    Create 26.js

---
+++ api/BabyTuru/26.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT26 Carlos",
+    "image" : "https://gateway.btfs.io/btfs/QmRK9YQyjmb3ozB8AftfBoV4Zwnawn3p2FBSnD6w46uV46"
+  })
+}
