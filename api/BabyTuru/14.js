c415d9b1ef2e4e2f095c36822c4445004e27e963
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:50:30 +0200

    Create 14.js

---
+++ api/BabyTuru/14.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT14 Debasish",
+    "image" : "https://gateway.btfs.io/btfs/QmTAvHZcmUvne4zJ6stD4BnCTKFLCjb1GcLdrCCSkjFqNP"
+  })
+}
