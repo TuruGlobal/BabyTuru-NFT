7fab4b56b18a4ba55cd2b2d01de1310492059ecd
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:38:37 +0200

    Create 31.js

---
+++ api/BabyTuru/31.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT31 Turu Yoda",
+    "image" : "https://gateway.btfs.io/btfs/QmWHX5GxPeHAstT8y8dpMMHhLoMY6jRcH5bz2L3GLwEKsR"
+  })
+}
