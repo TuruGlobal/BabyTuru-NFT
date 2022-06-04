b5c7a47f22fe52bfeeaff2c165d03134ea29b1c2
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:27:56 +0200

    Create 2.js

---
+++ api/BabyTuru/2.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT2 Carpie",
+    "image" : "https://gateway.btfs.io/btfs/QmV1pWyevvVz2nmkeodbdy7ubiYEMZAqWS7LbNv6dD5hfC"
+  })
+}
