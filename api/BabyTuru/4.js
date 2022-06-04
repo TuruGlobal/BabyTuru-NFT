4b1787cc244c04525d642b41c03b771d71d1113f
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:31:14 +0200

    Create 4.js

---
+++ api/BabyTuru/4.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT4 Davie",
+    "image" : "https://gateway.btfs.io/btfs/QmPnkQmJPoSnEZvhZpTw6Lekg57Y5yhnbjaezFq3UsqJNM"
+  })
+}
