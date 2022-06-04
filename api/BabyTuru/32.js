a6dd311fd9a2707970879513ac3a030a5d1c688f
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:40:27 +0200

    Create 32.js

---
+++ api/BabyTuru/32.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT32 Pirate Carpie",
+    "image" : "https://gateway.btfs.io/btfs/QmdfmXQizWKcsMabJUAqBFeoUY3LvQYo6wjRUSvbEsmDCi"
+  })
+}
