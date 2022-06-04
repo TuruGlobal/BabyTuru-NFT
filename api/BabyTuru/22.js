fc6c789e52d8ef4335a45142ad80848e38677780
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:02:27 +0200

    Create 22.js

---
+++ api/BabyTuru/22.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT22 Rob",
+    "image" : "https://gateway.btfs.io/btfs/Qme6ZX5umRhbvwsM5Vv29R8NrqrLpqciL7SKa2UUYiCf1b"
+  })
+}
