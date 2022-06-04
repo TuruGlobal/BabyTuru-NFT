ac185fbeeca9f01f27867a9f042030366cafc9f3
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:48:59 +0200

    Create 13.js

---
+++ api/BabyTuru/13.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT13 King Stephen",
+    "image" : "https://gateway.btfs.io/btfs/Qmc6TMCE4LaXyNtk8B14LqJn7C2JntsT5SwoES3zBuEpeY"
+  })
+}
