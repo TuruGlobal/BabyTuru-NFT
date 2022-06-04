5ee28587ea1ba0aa21a1c404908c02bda2b96c76
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:57:44 +0200

    Create 34.js

---
+++ api/BabyTuru/34.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT34 Robert",
+    "image" : "https://gateway.btfs.io/btfs/QmSehpcsGeKq8kxGYxnx9CqVDr4XBHQ7vFUGsSETg873TL"
+  })
+}
