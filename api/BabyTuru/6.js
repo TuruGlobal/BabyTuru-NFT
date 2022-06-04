3641f7a362e602f2b5a2224d8e38d58ec1b3ef7c
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:33:21 +0200

    Create 6.js

---
+++ api/BabyTuru/6.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT6 Chris",
+    "image" : "https://gateway.btfs.io/btfs/QmRUMe6nFGN7t6UJEvf7LKCGCm2QUxZEBixbY3vTPjcR5Z"
+  })
+}
