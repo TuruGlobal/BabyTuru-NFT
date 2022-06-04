86519fcf752789bac738e41f586549f7535c6ffc
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:56:42 +0200

    Create 19.js

---
+++ api/BabyTuru/19.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT19 Igor",
+    "image" : "https://gateway.btfs.io/btfs/QmegGYkuFebWj4c8kF8r19y1rvwhePs4nhHJYHb957T2ZN"
+  })
+}
