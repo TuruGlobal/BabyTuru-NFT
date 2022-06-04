5e84b711cfba669c46f7405b9de41d5292f15e87
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 15:08:13 +0200

    Create 36.js

---
+++ api/BabyTuru/36.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT36 Khabib",
+    "image" : "https://gateway.btfs.io/btfs/QmP84erFp6QrTadF6Z83cLHAfJqqEuBaiygTkizaNUNdGG"
+  })
+}
