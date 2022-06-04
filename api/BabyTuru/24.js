883a22ee0bc9792936721dbd011913b00d384946
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:08:17 +0200

    Create 24.js

---
+++ api/BabyTuru/24.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT24 Satyam",
+    "image" : "https://gateway.btfs.io/btfs/QmUbGt8PVuBwD2jVt3pGYTDLmBSHJoMoAwTbS7Xn5ViLCe"
+  })
+}
