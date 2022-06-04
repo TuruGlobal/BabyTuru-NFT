83b83df4fe0bcde078e296df5cc1141b0cbc8152
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 15:03:01 +0200

    Create 35.js

---
+++ api/BabyTuru/35.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT35 Turu Vader",
+    "image" : "https://gateway.btfs.io/btfs/QmQE7xyMSiCj8tUxyMrX1wUVoiXGSQVPjXFor5cZagcojo"
+  })
+}
