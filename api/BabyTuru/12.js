f8ea56fcb8bc36304574e4be9c72907046defda5
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:47:57 +0200

    Create 12.js

---
+++ api/BabyTuru/12.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT12 Dizzy Jay",
+    "image" : "https://gateway.btfs.io/btfs/QmcgT9uT3KoccWakRrdqYMrxctPjq8LX5uiwjqcHfid4pU"
+  })
+}
