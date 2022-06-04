7ab0cc1949643fc5fa1cb097f33dea7c3f9a8fb1
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 15:09:34 +0200

    Create 37.js

---
+++ api/BabyTuru/37.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT37 Storm Turu",
+    "image" : "https://gateway.btfs.io/btfs/QmXZxYSMEqGNMKtwdUcVJMi2VMg426Boj3XXSXGLdLUJ9g"
+  })
+}
