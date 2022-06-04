1cf92f3d7c17aa0f7a4ab66bbfb686a551086938
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 12:47:14 +0200

    Update 1.js

--- api/BabyTuru/1.js
+++ api/BabyTuru/1.js
@@ -1,1 +1,8 @@
-Hello
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "#BT1 Justin Sun",
+    "image" : "https://gateway.btfs.io/btfs/QmbHgCimAu46kRWrLeB7dv66Ji4Yy5Tjdkuc4FgG1fAzky"
+  })
+}
