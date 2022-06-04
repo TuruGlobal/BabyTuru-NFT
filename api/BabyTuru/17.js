e9bcc581faaaafb38d539386019b9ca78bd8bd8f
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:54:02 +0200

    Create 17.js

---
+++ api/BabyTuru/17.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT17 Nate",
+    "image" : "https://gateway.btfs.io/btfs/QmP5b5jKsnCG2SG8zyGP4eRD2CQpTv3DdiAjAKfwfevUaq"
+  })
+}
