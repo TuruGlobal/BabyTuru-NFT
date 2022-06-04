77e28421c516276b96cce39b2d644d9576ab036e
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:53:02 +0200

    Create 16.js

---
+++ api/BabyTuru/16.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT16 Vladimir",
+    "image" : "https://gateway.btfs.io/btfs/QmQzZYXYYbwUVsQWYz9Pit9yuxb4v3hF5jjjo53kgU8U2J"
+  })
+}
