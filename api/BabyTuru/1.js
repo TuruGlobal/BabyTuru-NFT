4ac0b1ee46721fa1a4550ad6ed4b38b5074c78e0
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 12:49:24 +0200

    Update 1.js

--- api/BabyTuru/1.js
+++ api/BabyTuru/1.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    "name"  : "#BT1 Justin Sun",
+    "name"  : "BT1 Justin Sun",
     "image" : "https://gateway.btfs.io/btfs/QmbHgCimAu46kRWrLeB7dv66Ji4Yy5Tjdkuc4FgG1fAzky"
   })
 }
