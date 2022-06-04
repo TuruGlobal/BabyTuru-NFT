73ff68c1dd8886fb485e3ea547054b121775afc1
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 14:33:08 +0200

    Create 27.js

---
+++ api/BabyTuru/27.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT27 Josip Broz Tito",
+    "image" : "https://gateway.btfs.io/btfs/QmThdTHinQkDNs4WdzcnLZJFxXdwDKceHg5AZpr46mpM2x"
+  })
+}
