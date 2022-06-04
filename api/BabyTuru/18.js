66fb3551e9c37a71665776b34da2ab3e17afcf74
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-04 13:55:40 +0200

    Create 18.js

---
+++ api/BabyTuru/18.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT18 iMad",
+    "image" : "https://gateway.btfs.io/btfs/QmePNVexjx28YE5mhERH1KyQbbWq8GoCvE9a6jQb9vx8S4"
+  })
+}
