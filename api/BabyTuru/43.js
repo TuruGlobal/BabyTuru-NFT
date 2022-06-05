e680a3e0b2292e7b9351983d5437a73f5f18f32f
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-05 12:24:59 +0200

    Create 43.js

---
+++ api/BabyTuru/43.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT43 Mike Tyson",
+    "image" : "https://gateway.btfs.io/btfs/QmYocRB8ox6QJtWsbHQMVfckuHrRSNrDBHh6YBExeN1ShC"
+  })
+}
