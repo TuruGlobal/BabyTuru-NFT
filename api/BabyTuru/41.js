7c99f2bd9d75006c9647bc2103e9ba502bfad2b8
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-05 12:21:50 +0200

    Create 41.js

---
+++ api/BabyTuru/41.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT41 Wolverine",
+    "image" : "https://gateway.btfs.io/btfs/QmNcHmeXSU8JLcyLazPL1JSgV7nXBX8XLAQQNa9ctxGcfo"
+  })
+}
