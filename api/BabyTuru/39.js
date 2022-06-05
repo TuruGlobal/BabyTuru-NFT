7386382915532fb9032aed3faa27047ceff0a7c8
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-05 12:19:13 +0200

    Create 39.js

---
+++ api/BabyTuru/39.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT39 Joker Turu",
+    "image" : "https://gateway.btfs.io/btfs/QmVyjraonS7VtY6NAXvX9nbBVqsAK2jDnfFZUX9sXokx2K"
+  })
+}
