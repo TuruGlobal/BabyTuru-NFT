4ff4eaa6ebd43416948a63ba1189e7b532be94ad
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-05 12:23:24 +0200

    Create 42.js

---
+++ api/BabyTuru/42.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT42 Boba Fett",
+    "image" : "https://gateway.btfs.io/btfs/QmViasat7gqXLWe6rxNFWtePZnbmYDj1nThGYWbNXxSLhK"
+  })
+}
