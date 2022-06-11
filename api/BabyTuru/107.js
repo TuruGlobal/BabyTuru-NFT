62c8a7e4219fa1fe131b4e9b03eb2e7e2423b09f
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-11 10:35:57 +0200

    Create 107.js

---
+++ api/BabyTuru/107.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC7 Andy Rackman",
+    "image" : "https://gateway.btfs.io/btfs/QmY1wLou91rLQL1x67oe7k12M2EM1mwzmkfXrdMB2RKqrH"
+  })
+}
