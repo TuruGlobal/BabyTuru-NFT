b07462398d306df209ab93ad1f50110bb3b737a8
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-11 10:25:21 +0200

    Create 106.js

---
+++ api/BabyTuru/106.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC6 Maria Tower",
+    "image" : "https://gateway.btfs.io/btfs/QmTV1bPJttx4yyZkDLNcZ2Z5KDEYvAACqLRzqkCMLXugcA"
+  })
+}
