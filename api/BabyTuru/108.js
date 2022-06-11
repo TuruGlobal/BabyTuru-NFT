317749f8d53baf307adaef390a7334e5a374b8b6
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-11 10:38:39 +0200

    Create 108.js

---
+++ api/BabyTuru/108.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC8 Sven Schmedemann",
+    "image" : "https://gateway.btfs.io/btfs/QmSJmep897wTnGUEGVN6n7UNUff9X6pKD3YntzjM5EmsdU"
+  })
+}
