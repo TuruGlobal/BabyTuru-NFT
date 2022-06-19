52ac56e5ec748e0ec8f8af6414ea6419f05f6407
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-19 11:01:53 +0200

    Create 109.js

---
+++ api/BabyTuru/109.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC9 Michael Eder",
+    "image" : "https://gateway.btfs.io/btfs/QmVRRywJBJb7c6g8APUaDzAYCAFhGcQPQL1b9CBfbS6UZj"
+  })
+}
