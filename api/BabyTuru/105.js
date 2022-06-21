6feda93b0351d979ab2cd3c990ca83c8b75e62fc
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-21 17:29:03 +0200

    Create 105.js

---
+++ api/BabyTuru/105.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC5 Schooley D",
+    "image" : "https://gateway.btfs.io/btfs/QmYbtGF3SEgYzCNxM41gJqXSNPPxQNkvwe7WPjXxgRvNFV"
+  })
+}
