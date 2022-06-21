84969a5ffc3a7671999aa0b1ea40505261b63be2
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-21 16:30:31 +0200

    Create 110.js

---
+++ api/BabyTuru/110.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC10 BabyNate",
+    "image" : "https://gateway.btfs.io/btfs/QmS9vmDDEUsiQYJdar5QjipVjsRQJDppw8AVqNzMifPTyo"
+  })
+}
