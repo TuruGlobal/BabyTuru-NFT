be0219ceb3ba2d04e1c73eacadb3256c64330016
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-21 17:23:48 +0200

    Create 102.js

---
+++ api/BabyTuru/102.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC2 Syed",
+    "image" : "https://gateway.btfs.io/btfs/QmNScFfjqZpa53vdf2Brbie5CBWrjyjHm33ur1YogqAAbt"
+  })
+}
