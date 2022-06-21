a922f75cd94497cb9dc1124b079bb6608132d27d
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-21 17:21:05 +0200

    Create 46.js

---
+++ api/BabyTuru/46.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT46 BabySonic",
+    "image" : "https://gateway.btfs.io/btfs/QmRc48fPdrgqFeUF6o3JtpdCLgTP3u7DaPx3W93yaw95Zy"
+  })
+}
