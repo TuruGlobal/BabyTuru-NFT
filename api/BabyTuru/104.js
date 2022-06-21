47ef1120f5e8ad6d5fc725b52b22e89ccbf8bcf8
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-21 17:27:44 +0200

    Create 104.js

---
+++ api/BabyTuru/104.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC4 Tron ITF",
+    "image" : "https://gateway.btfs.io/btfs/QmQoNcw9qFQU2YqgroX6JeGF51MXUp8MKHkv5SPFsyLypM"
+  })
+}
