ac4ebd84edb54d09341399678c909d65aa290047
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-06-21 17:22:33 +0200

    Create 101.js

---
+++ api/BabyTuru/101.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "MC1 Lars",
+    "image" : "https://gateway.btfs.io/btfs/QmRutBFrSnXE3Kwksb6c4tyDqNAR9FqMFCFmWkqeQWBubY"
+  })
+}
